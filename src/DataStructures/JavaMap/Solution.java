02d359b2369f5828820fe9a6693f3a22d7331dc6
Amer Almounajjed <76974168+Al-Amer@users.noreply.github.com>
2023-12-02 21:05:21 +0100

    Add files via upload

--- src/DataStructures/JavaMap/Solution.java
+++ src/DataStructures/JavaMap/Solution.java
@@ -1,2 +1,31 @@
-package DataStructures.JavaMap;public class Solution {
+package java.DataStructures.JavaMap;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        in.nextLine();
+        for(int i=0;i<n;i++)
+        {
+            String name=in.nextLine();
+            int phone=in.nextInt();
+            in.nextLine();
+        }
+        while(in.hasNext())
+        {
+            String s=in.nextLine();
+        }
+
+    }
+
 }
+
+/*
+
+https://www.hackerrank.com/challenges/phone-book/problem
+
+ */

f88348beec283fc88dfe83f6d440299d6ccf4899
Amer Almounajjed <76974168+Al-Amer@users.noreply.github.com>
2023-12-03 01:40:42 +0100

    Add files via upload

---
+++ src/String/ValidUsernameRegularExpression/Solution.java
@@ -0,0 +1,25 @@
+package Java.String.ValidUsernameRegularExpression;
+import java.util.Scanner;
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+  //  public static final String regularExpression = "^[a-zA-Z][\\w]{7,29}$";  ^[a-zA-Z][\\w]{7,29}
+    public static final String regularExpression = "^[A-Za-z][A-Za-z0-9_]{7,29}$";
+}
+public class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
+//  https://www.hackerrank.com/challenges/valid-username-checker/problem?isFullScreen=true

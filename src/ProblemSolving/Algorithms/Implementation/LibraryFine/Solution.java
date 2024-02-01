65518a3ccd12454d3c59ad68d604393a18732678
Amer Almounajjed <76974168+Al-Amer@users.noreply.github.com>
2024-02-01 10:13:42 +0100

    Add files via upload

--- src/ProblemSolving/Algorithms/Implementation/LibraryFine/Solution.java
+++ src/ProblemSolving/Algorithms/Implementation/LibraryFine/Solution.java
@@ -1,1 +1,104 @@
+/*
+Your local library needs your help! Given the expected and actual return dates for a library book, create a program
+that calculates the fine (if any). The fee structure is as follows:
+1. If the book is returned on or before the expected return date, no fine will be charged (i.e.: fine = 0).
+2. If the book is returned after the expected return day but still within the same calendar month and year as the
+expected return date,
+fine = 15 Hackos * (the number of days late).
+3. If the book is returned after the expected return month but still within the same calendar year as the expected
+return date, the
+fine = 500 Hackos * (the number of months late).
+4. If the book is returned after the calendar year in which it was expected, there is a fixed fine of 10000 Hackos.
+
+Charges are based only on the least precise measure of lateness. For example, whether a book is due January 1, 2017 or
+December 31, 2017, if it is returned January 1, 2018, that is a year late and the fine would be 10000 Hackos.
+ */
+
+package ProblemSolving.Algorithms.Implementation.LibraryFine;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'libraryFine' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER d1
+     *  2. INTEGER m1
+     *  3. INTEGER y1
+     *  4. INTEGER d2
+     *  5. INTEGER m2
+     *  6. INTEGER y2
+     */
+
+    public static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+        // Write your code here
+        int reult =0;
+        int year =  y1 - y2;
+        int month = m1 - m2;
+        if (year < 0){
+            reult = 0;
+        }else if (year > 0){
+            reult = 10000;
+        }else {
+            if (month < 0){
+                reult =0;
+            }else if (month > 0){
+                reult = (month * 500) ;
+            }else {
+                int day = (d1 > d2)? d1-d2 : 0;
+                reult = day * 15;
+            }
+        }
+        return reult;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int m1 = Integer.parseInt(firstMultipleInput[1]);
+
+        int y1 = Integer.parseInt(firstMultipleInput[2]);
+
+        String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d2 = Integer.parseInt(secondMultipleInput[0]);
+
+        int m2 = Integer.parseInt(secondMultipleInput[1]);
+
+        int y2 = Integer.parseInt(secondMultipleInput[2]);
+
+        int result = Result.libraryFine(d1, m1, y1, d2, m2, y2);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+
+        bufferedReader.close();
+//        bufferedWriter.close();
+        // outprint method to show the result
+        System.out.println(result);
+
+    }
+}
+
 

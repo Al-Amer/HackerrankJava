624b07e80716ccab6409340b3b1483cfef8999cd
Amer Almounajjed <76974168+Al-Amer@users.noreply.github.com>
2024-02-06 12:11:43 +0100

    Add files via upload

--- src/ProblemSolving/Algorithms/Implementation/ServiceLane/Solution.java
+++ src/ProblemSolving/Algorithms/Implementation/ServiceLane/Solution.java
@@ -1,3 +1,22 @@
+/*
+A driver is driving on the freeway. The check engine light of his vehicle is on, and the driver wants to
+get service immediately. Luckily, a service lane runs parallel to the highway.
+It varies in width along its length.
+You will be given an array of widths at points along the road (indices), then a list of the indices of
+entry and exit points. Considering each entry and exit point pair, calculate the maximum size vehicle that
+can travel that segment of the service lane safely.
+
+Example
+
+n = 4
+width = [2, 3, 2, 1]
+cases = [[1, 2], [2, 4]]
+
+If the entry index, i = 1 and the exit, j = 2, there are two segment widths of 2 and 3 respectively.
+The widest vehicle that can fit through both is 2. If i = 2 and j = 4,
+the widths are [3, 2, 1] which limits vehicle width to 1.
+ */
+
 package ProblemSolving.Algorithms.Implementation.ServiceLane;
 
 import java.io.*;

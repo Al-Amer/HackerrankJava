ce7000808ab9d04fd6602eae4581664222e604dd
Amer Almounajjed <76974168+Al-Amer@users.noreply.github.com>
2024-02-17 02:16:09 +0100

    Add files via upload

--- src/ProblemSolving/Algorithms/Implementation/TaumAndBDay/Solution.java
+++ src/ProblemSolving/Algorithms/Implementation/TaumAndBDay/Solution.java
@@ -1,1 +1,86 @@
+/*
+Taum is planning to celebrate the birthday of his friend, Diksha.
+There are two types of gifts that Diksha wants from Taum: one is black and the other is white.
+To make her happy, Taum has to buy b black gifts and w white gifts.
+The cost of each black gift is bc units.
+The cost of every white gift is wc units.
+The cost of converting each black gift into white gift or vice versa is z units.
+Help Taum by deducing the minimum amount he needs to spend on Diksha’s gifts.
+Input Format
+The first line will contain an integer t, the number of test cases.
+The next t pairs of lines are as follows: — The first line contains the values of integers b and w .
+The next line contains the values of integers bc, wc, and z.
+ */
+
+package ProblemSolving.Algorithms.Implementation.TaumAndBDay;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'taumBday' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER b
+     *  2. INTEGER w
+     *  3. INTEGER bc
+     *  4. INTEGER wc
+     *  5. INTEGER z
+     */
+
+    public static long taumBday(int b, int w, int bc, int wc, int z) {
+        // Write your code here
+        return (long) (Math.min((long)(wc+z),bc)*b + Math.min(wc,(long)(bc+z))*w);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int b = Integer.parseInt(firstMultipleInput[0]);
+
+                int w = Integer.parseInt(firstMultipleInput[1]);
+
+                String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int bc = Integer.parseInt(secondMultipleInput[0]);
+
+                int wc = Integer.parseInt(secondMultipleInput[1]);
+
+                int z = Integer.parseInt(secondMultipleInput[2]);
+
+                long result = Result.taumBday(b, w, bc, wc, z);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
 
